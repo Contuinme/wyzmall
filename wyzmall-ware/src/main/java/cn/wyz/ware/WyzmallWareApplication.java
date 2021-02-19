a9b4cb89e8ba09f9f0ee66be02cb094d298129a5
wangnanxiang <806543499@qq.com>
2021-02-19 21:52:50 +0800

    项目基础环境搭建

---
+++ wyzmall-ware/src/main/java/cn/wyz/ware/WyzmallWareApplication.java
@@ -0,0 +1,13 @@
+package cn.wyz.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyzmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WyzmallWareApplication.class, args);
+    }
+
+}
