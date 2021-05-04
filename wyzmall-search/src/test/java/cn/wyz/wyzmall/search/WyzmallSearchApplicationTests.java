d201f2acec2d904699ebbc9e0d1f4794a36c44cd
wangnanxiang <806543499@qq.com>
2021-05-05 01:48:13 +0800

    前端编译文件

---
+++ wyzmall-search/src/test/java/cn/wyz/wyzmall/search/WyzmallSearchApplicationTests.java
@@ -0,0 +1,19 @@
+package cn.wyz.wyzmall.search;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyzmallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClientl;
+
+    @Test
+    void contextLoads() {
+        System.out.println(restHighLevelClientl);
+    }
+
+}
